cbe507dff56e7e648a7f0b7f797b30e7ff5e49c8
thecsguy09 <thecsguy09@gmail.com>
2025-10-07 01:29:05 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { store } from './redux/store'
 import App from './App'
 import BatchPage from './components/BatchPage'
